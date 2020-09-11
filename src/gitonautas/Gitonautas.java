6f8c9609648bc098f244e02b210b4864cf32309f
Mark Zamudio <mark.taix.developer@hotmail.com>
2020-09-11 14:46:28 -0500

    Estructura inicial del projecto

---
+++ src/gitonautas/Gitonautas.java
@@ -0,0 +1,12 @@
+package gitonautas;
+
+/**
+ *
+ * @author Gitonautas
+ */
+
+public class Gitonautas {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido Al Projecto");
+    }
+}
